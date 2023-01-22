6c5528533a18e488d0797bc4918b8cbbab4b7a54
Thomas Boggs <thomboggs@gmail.com>
2023-01-22 14:08:06 -0500

    Initial Commit

---
+++ Elev8r/Elev8rApp.swift
@@ -0,0 +1,17 @@
+//
+//  Elev8rApp.swift
+//  Elev8r
+//
+//  Created by Thomas Boggs on 1/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Elev8rApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
